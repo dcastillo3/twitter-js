86d5e3db56be79c5d73640cad022e97ae78a6695
Daniel Castillo <dcastillo3@gmail.com>
2018-04-19 11:43:29 -0400

    updated twitter

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require( 'express' );
+const chalk = require('chalk');
+const volleyball = require('volleyball')
 const app = express(); // creates an instance of an express application
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use('./', function (req, res, next) {
+     res.send(200)
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+})
 
-app.listen(3000, () => console.log('Twitter-js listening on port 3000!'))
+
+app.use('/volleyball', function(req,res,next){
+    res.send('You reached the special area!')
+ });
+// app.get('/volleyball', (req, res) => res.send('You reached the special area!'))
+
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log(chalk.green('Twitter-js listening on port 3000!')))
+
